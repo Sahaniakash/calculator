172c3e4957132fdd82902f92d02bc5f5aa290bfa
Akash Sahani <sahaniakash511@gmail.com>
2025-03-08 17:29:49 +0530

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-let string = "";
-let buttons = document.querySelectorAll ('.button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            document.querySelector('input').value= string;
+function appendValue(value) {
+            document.getElementById('display').value += value;
         }
-       else if(e.target.innerHTML == 'AC'){
-            string = "";
-            document.querySelector('input').value= string;
-       }
-        else{
-            console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value= string;
+        function clearDisplay() {
+            document.getElementById('display').value = '';
         }
-
-    })
-})
+        function calculate() {
+            try {
+                document.getElementById('display').value = eval(document.getElementById('display').value);
+            } catch {
+                document.getElementById('display').value = 'Error';
+            }
+        }
